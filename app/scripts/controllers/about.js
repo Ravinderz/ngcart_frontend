4efb8f0fbded6859cb1f06c04cb556ba5c86bd82
Ravinder Singh Jagtar <jravinder@pkrmsezdc.com>
2017-04-03 17:17:30 +0530

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngCartAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ngCartAppApp
+ */
+angular.module('ngCartAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngCartAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ngCartAppApp
+ */
+angular.module('ngCartAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
